7d8399ffd67c1a182a60b7e6ce45ba7c4fbb5206
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-16 09:56:10 -0700

    C if,else,while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    char i;
+
+    char T[26] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
+    for (i = T[0]; i != '\0' ; i++)
+	putchar(i);	
+    return (0);
+}
