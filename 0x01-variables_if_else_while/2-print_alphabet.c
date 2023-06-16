da88c980f057bdf89ad9b51a77a119c4c5eb0c48
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-16 10:11:16 -0700

    C if,else,while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,12 @@
 
 int main(void)
 {
-    int i;
-
-    char T[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
-    for (i=0; i < 26 ; i++)
-    putchar (T[i]);	    
-    return (0);
+	int i;
+	char T[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+	for (i=0; i < 26 ; i++)
+	{
+		putchar(T[i]);
+	}
+	putchar('\n');
+	return (0);
 }
