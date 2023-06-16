37ce5a0580bb75997e83112c60947a6790fef917
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-16 10:39:59 -0700

    C if,else,while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char t[9] = "0123456789";
+	for (i = 0; i <10 ; i++)
+	putchar(t[i]);
+	putchar("\n");
+	return (0);
+}
