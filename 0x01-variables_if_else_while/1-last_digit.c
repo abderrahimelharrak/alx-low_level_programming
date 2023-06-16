a3cd2fc34cd8b61af7a18e567e30572fab983fc3
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-16 09:17:06 -0700

    C if,else,while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        return (0);
