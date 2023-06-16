74b4b7af530cba9833a0576579b87973c1968957
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-16 10:24:06 -0700

    C if,else,while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int i;
+        char T[26] = "abcdefghijklmnopqrstuvwxyz";
+
+        for (i = 0; i < 26 ; i++)
+        {
+                putchar(T[i]);
+		putchar('\n');
+	}
+        return (0);
+}
+~      
