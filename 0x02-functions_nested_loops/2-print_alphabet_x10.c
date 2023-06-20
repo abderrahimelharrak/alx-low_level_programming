8fb79c4090012cca3f03bbc9f18021f505fffe0f
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-20 16:49:48 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+        int i;
+        char T[26] = "abcdefghijklmnopqrstuvwxyz";
+	int j = 0;
+	
+	while (j < 10)
+	{
+		for (i = 0 ; i < 26 ; i++)
+                _putchar(T[i]);
+        	j++;
+	}
+        _putchar('\n');
+}
