0ed8a053855c7b7d1379b2c16194d8c1c0fb5b91
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-20 16:37:26 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
-/* more headers goes there */
+
 /**
  * main - Entry point
  *
- * Return : Always 0
  */
+
 void print_alphabet(void)
 {
 	int i;
 	char T[26] = "abcdefghijklmnopqrstuvwxyz";
+
 
 	for (i = 0 ; i < 26 ; i++)
 	{
