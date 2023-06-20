0ac1c47ef478022d615d38794b36667d41ae1bba
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-20 16:55:42 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet_x10 - print all alphabet x10 in lowercase
  */
 
 void print_alphabet_x10(void)
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int i;
 	char T[26] = "abcdefghijklmnopqrstuvwxyz";
 	int j = 0;
-	
+
 	while (j < 10)
 	{
 		for (i = 0 ; i < 26 ; i++)
