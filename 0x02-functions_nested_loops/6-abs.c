6bf120da30593544dabee87db17bb36c054e1e9d
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-20 17:59:01 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * int _abs - computes the absolut value of an integer
+ * _abs - computes the absolut value of an integer
  * @i: the integer to compute
  * Return: i for positive -i for negative
  */
 int _abs(int i)
 {
-	if (i < 0 )
+	if (i < 0)
 		return (-i);
 	return (i);
 }
