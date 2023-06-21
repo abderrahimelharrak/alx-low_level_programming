45c515db670155b446236354ad4bd8964ba16ea0
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-20 17:36:31 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 /**
  * _isalpha - check the caracter is alphabetic
  * @c : the caracter to check
- * Return : 1 if the caracter is alphabetic 0 if not
+ * Return : 1 the caracter is alphabetic 0 if not
  */
 int _isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-			return (1);
+		return (1);
 	return (0);
 }
