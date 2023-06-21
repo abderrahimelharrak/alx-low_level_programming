479bf3861e42114de9ddc7b0a40013302ec16b00
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-20 17:08:44 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * Prototype: int _islower(int c);
- * Returns 1 if c is lowercase
- * Returns 0 otherwise
+ * int _islower(int c)- Returns 1 if c is lowercase  Returns 0 otherwise
  */
 int _islower(int c)
 {
