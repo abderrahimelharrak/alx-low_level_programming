442a010d1fdf045621e503cb96b605296f9f1849
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-22 16:35:49 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * _isdigit - check for a digit
  * @c: the degit to check
- * Return: 1 gor digit 0 for any else
+ * Return: 1 for digit 0 for any else
  */
 
 int _isdigit(int c)
 {
-	if (c > 9 && c <= 0)
+	if (c <= 0 && c > 9)
 		return (1);
 	return (0);
 }
