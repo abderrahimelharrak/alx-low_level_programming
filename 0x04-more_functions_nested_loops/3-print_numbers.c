510f31a30875f0087a20d1ec03befe53bb4484c6
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-22 16:57:13 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10 ; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
