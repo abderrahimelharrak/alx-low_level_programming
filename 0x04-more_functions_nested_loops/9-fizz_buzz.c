a037b35919ddb870f350054a50fa80d01eaecead
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-22 19:00:42 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - print the numbers
- * Return: 0 seccess
+ * main - prints the numbers from 1 to 100
+ * 3 multiples print Fizz instead of the number
+ * 5 multiples print Buzz instead of the number
+ * 3 & 5 multiples print FizzBuzz instead of the number
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
+	int x;
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char FB[] = "FizzBuzz";
 
-	for (n = 1 ; n <= 100 ; n++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (n % 3 != 0 && n % 5 != 0)
-		printf("%d ", n);
-		if (n % 3 == 0 && n % 5 == 0)
-		{
-			printf("FizzBuzz ");
-			continue;
-		}
-		if (n % 3 == 0)
-		printf("Fizz ");
-		if (n % 5 == 0)
-		printf("Buzz ");
+		if (x == 100)
+			printf("%s", buzz);
+		else if ((x % 3 == 0) && (x % 5 == 0))
+			printf("%s ", FB);
+		else if (x % 3 == 0)
+			printf("%s ", fizz);
+		else if (x % 5 == 0)
+			printf("%s ", buzz);
+		else
+			printf("%d ", x);
 	}
 	printf("\n");
 	return (0);
