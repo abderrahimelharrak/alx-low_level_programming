d6010ad28889a58335280bc02422a680f2c5c848
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-22 17:57:17 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,9 +11,12 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (i = 1 ; i <= n ; i++)
+	else
 	{
-		_putchar('_');
+		for (i = 1 ; i <= n ; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
