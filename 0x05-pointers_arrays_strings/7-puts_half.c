ab07a6c01675e103dbcbcf2069254199c0cdaeb3
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-26 19:02:21 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+
+/**
+ * puts_half - function that prints half of a string
+ * @str: the string to print
+ */
+
+void puts_half(char *str)
+{
+	int i = _strlen(str);
+
+	x = (i / 2);
+	for ( ; *(s + x) != '\0' ; x++)
+	{
+		_putchar(*(s + x));
+	}
