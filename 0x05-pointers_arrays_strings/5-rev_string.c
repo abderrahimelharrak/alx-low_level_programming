a130f0fb36b8ff5844248a03255dd6496dd85d64
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-26 18:33:58 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,17 @@
 
 void rev_string(char *s)
 {
-	print_rev(s);
+	char swch = *(s + 0);
+		int y = 0;
+		int z;
+
+		while (*(s + y) != '\0')
+			y++;
+		for (z = 0; z < y; z++)
+		{
+			y--;
+			swch = *(s + z);
+			*(s + z) = *(s + y);
+			*(s + y) = rev;
+		}
 }
