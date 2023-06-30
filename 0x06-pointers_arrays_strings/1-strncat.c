7fe8bb97bf7d1e850b7ffb635f21ef6c9a400301
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-06-30 16:23:59 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: the string to copy to
+ * @src: the string to copy from
+ * @n: the number of bytes to copy
+ * Return: two concat strings
+ */
+char *_strcat(char *dest, char *src)
+{
+
+        int i, y;
+
+        for (i = 0; dest[i] != '\0'; i++)
+                ;
+
+        for (y = 0; src[y]  != '\0' && y < n ; n++, i++)
+        {
+                dest[i] = src[n];
+        }
+        dest[i] = '\0';
+        return (dest);
+}
