fb50a20f11668533707050091c254bb8b848a1d9
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-07-05 19:09:06 -0700

    task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power recursion
+ * @x: input
+ * @y: input
+ * Return: an int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	
+	if(x != 0 && y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x,y - 1));
+}
