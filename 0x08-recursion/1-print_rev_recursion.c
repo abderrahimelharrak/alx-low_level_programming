db86b4fb39f9635f7a119f741835584c5d7bf852
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-07-05 18:49:24 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,13 +7,11 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')	
-	{
-		s++;
-		_print_rev_recursion(s);
-		s--;
-		_putchar(*s);
-	}
-	else
-		_putchar('\n');
+	if (*s == '\0')
+		return;
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
