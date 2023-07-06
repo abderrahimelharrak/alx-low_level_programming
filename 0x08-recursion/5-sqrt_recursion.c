496695b903e0e52f96bf384dfd6d13b39968f70d
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-07-05 19:26:13 -0700

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion = sqrt
+ * @n: number
+ * Return: sqrt of number n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n,1));
+}
+
+/**
+ * _sqrt - sqrt
+ * @n: input
+ * @i: input
+ * Return: sqrt
+ */
+
+int _sqrt(int n, int i)
+{
+	int s = i * i;
+
+	if (s > n)
+		return (-1);
+
+	if (s == n)
+		return (i);
+
+	return (_sqrt(n,i+1);
+
+}
