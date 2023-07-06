a06cd6a4f84dc74bf8afde4647f913d1ae9aa4b7
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-07-05 19:11:56 -0700

    task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,10 +11,9 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	
 	if (x != 0 && y == 0)
 		return (1);
 
-	return (x * _pow_recursion(x,y - 1));
+	return (x * _pow_recursion(x, y - 1));
 
 }
