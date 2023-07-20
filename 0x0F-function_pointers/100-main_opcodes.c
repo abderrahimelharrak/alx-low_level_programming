4a1815dc515a6ab5b0c05de699493a35e89ee4f0
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-07-19 18:37:23 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,1 +1,33 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: the number of args
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	char *t = (char *)main;
+	int x;
+
+	if (argc != 2)
+	{
+		printf(" no arguments .... error\n");
+		exit(0);
+	}
+	x = atoi(argv[1]);
+	if (x < 0)
+	{
+		printf("no arg vecteur ..... error \n");
+		exit(0);
+	}
+	while (x--)
+	{
+		printf("%02hhx%s", *t++, x ? " " : "\n");
+	}
+	return (0);
+}
