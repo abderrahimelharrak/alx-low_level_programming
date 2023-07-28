072f3faffb69d6dd442469ee215c0b02355506c7
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-07-27 17:42:11 -0700

    task_5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * tortue - prints a phrase before the main
+ */
+void tortue(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
