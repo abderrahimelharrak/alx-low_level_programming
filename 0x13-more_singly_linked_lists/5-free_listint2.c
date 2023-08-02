d456d9cac29ddfbb4e8f852925d2cef6156b2907
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-08-01 18:35:53 -0700

    all_tasks

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,24 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Write a function that frees a listint_t list.
- * @head: the input struct
- * Return: success
+ * free_listint2 - free a linked list
+ * @head: the list to free
  */
 
 void free_listint2(listint_t **head)
 {
-        listint_t *list;
+	listint_t *list;
 
-        if (head == NULL)
-        return (NULL);
-        while (*head)
-        {
-                list = (*head)->next;
-                free(*head);
-                *head = list;
-        }
+	if (head == NULL)
+	return (NULL);
 
-        *head = NULL;
-        }
+	while (*head)
+	{
+		list = (*head)->next;
+		free(*head);
+		*head = list;
+	}
+
+	*head = NULL;
 }
+
