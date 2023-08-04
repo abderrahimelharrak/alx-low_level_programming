8d817928c44f088691b945359c7242d7b0eab748
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-08-03 17:05:57 -0700

    task_0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,9 +15,7 @@ unsigned int binary_to_uint(const char *b)
 
 	while (*b)
 	{
-		if (*b != '0')
-			return (0);
-		if (*b != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
 	}
 	x = x * 2 + (*b++ - '0');
