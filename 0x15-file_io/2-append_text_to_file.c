9a2a13a24b6cbecd80f0eaaee3e166ae99b39ec4
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-08-07 18:30:17 -0700

    task_2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		length = 0;
-	while (*text_length)
+	while (*text_content)
 		length++;
 	if (!filename)
 		return (-1);
