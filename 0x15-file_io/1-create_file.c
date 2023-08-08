13cf232173ac8a075b00be04362e9d4249fa9299
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-08-07 17:11:48 -0700

    task_1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and puts text in it
+ * @filename: name of the file
+ * @text_content: text in the file
+ * Return: 1 if it successed 0 otherwise
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int x_fl;
+	int len, inlength;
+	char *tab;
+
+	len = 0;
+	inlength = 0;
+	if (filename == NULL)
+		return (-1);
+
+	x_fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	
+	if (x_fl == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (inlength = 0, tab = text_content; *tab; tab++)
+			inlen++;
+		len = write(x_fl, text_content, inlength);
+	}
+
+	if (close(x_fl) == -1 || inlength != len)
+		return (-1);
+
+	return (1);
+}
