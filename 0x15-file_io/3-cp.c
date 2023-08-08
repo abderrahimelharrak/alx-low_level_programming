76fb65cdc4d03a5e6350ec8f5a74153333d35658
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-08-07 18:48:03 -0700

    task_3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -63,44 +63,44 @@ int read_error(int file1, int file2, char *file_name)
 
 /**
  * main - main code
- * @ac: teh number of arg
- * @av: the list of args
+ * @argc: teh number of arg
+ * @argv: the list of args
  * Return: the result
  */
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
 	char tab[1024];
 	int length_r, length_w, f_from, f_to, error;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
-	f_from = open(av[1], O_RDONLY);
+	f_from = open(argv[1], O_RDONLY);
 
 	if (f_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			av[1]);
+			argv[1]);
 		return (98);
 	}
-	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
+	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (f_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close_file(f_from);
 		return (99);
 	}
 	do {
 		length_r = read(f_from, tab, 1024);
 		if (length_r == -1)
-			return (read_error(file_from, file_to, av[1]));
+			return (read_error(file_from, file_to, argv[1]));
 
 		length_w = write(f_to, tab, length_r);
 		if (length_w == -1 || length_w != length_r)
-			return (write_error(f_from, f_to, av[2]));
+			return (write_error(f_from, f_to, argv[2]));
 	} while (length_r == 1024);
 
 	error = close_file(f_from);
