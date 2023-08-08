f024a1700dc124030f178db389206f4aed9bc1c4
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-08-07 17:44:16 -0700

    task_2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,24 +9,24 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fichier;
-	int x, length = 0;
+        int fichier;
+        int x, length = 0;
 
-	if (filename == NULL)
-		return (-1);
+        if (filename == NULL)
+                return (-1);
 
-	fichier = open(filename, O_WRONLY | O_APPEND);
-	if (text_content != NULL)
-	{
-		while (text_content[length])
-			length++;
-	}
-	x = write(fichier, text_content, length);
-	if (fichier == -1)
-		return (-1);
-	if (x == -1)
-		return (-1);
-	clode(fichier);
-	return (1);
+        fichier = open(filename, O_WRONLY | O_APPEND);
+        if (text_content != NULL)
+        {
+                while (text_content[length])
+                        length++;
+        }
+        x = write(fichier, text_content, length);
+        if (fichier == -1)
+                return (-1);
+        if (x == -1)
+                return (-1);
+        clode(fichier);
+        return (1);
 
 }
