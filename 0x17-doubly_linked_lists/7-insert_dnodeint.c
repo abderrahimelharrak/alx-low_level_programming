d52a117d281e285656e972ca4815fd227a1260f8
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-09-14 15:51:07 -0700

    task_7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = tmp->next;
 	new_node->prev = tmp;
-	if (temp->next != NULL)
+	if (tmp->next != NULL)
 		tmp->next->prev = new_node;
 	tmp->next = new_node;
 	return (new_node);
